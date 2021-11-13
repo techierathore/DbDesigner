7d861bafabd9764e64856f4c71718053c9f8eff2
S Ravi Kumar <techierathore@gmail.com>
2021-11-13 20:57:37 +0530

    Add project files.

---
+++ DbDesigner.Models/Column.cs
@@ -0,0 +1,24 @@
+﻿namespace DbDesigner.Models
+{
+    public class Column
+    {
+        public event Action Changed;
+
+        public string Name { get; set; }
+        public ColumnType Type { get; set; }
+        public bool Primary { get; set; }
+
+        public void Refresh() => Changed?.Invoke();
+    }
+
+    public enum ColumnType
+    {
+        Boolean,
+        Char,
+        String,
+        SByte,
+        Short,
+        Integer,
+        Long
+    }
+}
---
+++ DbDesigner.UI/Components/TableNode.razor.cs
@@ -0,0 +1,10 @@
+﻿using DbDesigner.Models;
+using Microsoft.AspNetCore.Components;
+namespace DbDesigner.UI.Components
+{
+    public partial class TableNode
+    {
+        [Parameter]
+        public Table Node { get; set; }
+    }
+}
